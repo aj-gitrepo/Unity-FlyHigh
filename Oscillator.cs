3e686d92b091a8c3d8935d29a3d4fdce15951a8c
wink-aajlak <aajlak.wink@gmail.com>
2022-07-20 13:11:24 +0530

    completed

--- Oscillator.cs
+++ Oscillator.cs
@@ -15,10 +15,14 @@ public class Oscillator : MonoBehaviour
 
     void Update()
     {
+        //comparing to 0 may not be accurate in float, so using the smallest float in Unity given by Mathf.Epsilon
+        if(period <= Mathf.Epsilon) return; //to prevent NaN Error 
+        const float tau = Mathf.PI * 2; //2pialue of 6.283
         float cycles = Time.time / period;
-        const float tau = Mathf.PI * 2; //2pi
         float rawSinWave = Mathf.Sin(cycles * tau); //angle in radians //generates value from -1 to 1
-        Debug.Log(rawSinWave);
+        
+        movementFactor = (rawSinWave + 1) / 2f; //(rawSinWave + 1) gives 0 - 2, divide by 2 gives 0 - 1
+
         Vector3 offset = movementVector * movementFactor;
         transform.position = startingPosition + offset;
     }
